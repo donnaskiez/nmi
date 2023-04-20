da263edd16f5e105af95c88ef5a47fee2634bc32
lhodges1 <lachiehodges7785@gmail.com>
2023-04-20 17:52:07 +1000

    nice :)

--- nmi-callback-handler/driver.c
+++ nmi-callback-handler/driver.c
@@ -14,11 +14,23 @@ typedef struct _KAFFINITY_EX
 
 } KAFFINITY_EX, * PKAFFINITY_EX;
 
+typedef struct _NMI_CALLBACK_DATA
+{
+	UINT64	kthread_address;
+	UINT64	start_address;	
+	UINT64	stack_limit;
+	UINT64	stack_base;
+	PVOID	stack_unwind_pool;
+	int		num_frames_captured;
+	UINT64	thread_cr3;
+
+}NMI_CALLBACK_DATA, *PNMI_CALLBACK_DATA;
+
+PVOID thread_data_pool;
+
 EXTERN_C VOID KeInitializeAffinityEx(PKAFFINITY_EX affinity);
 EXTERN_C VOID KeAddProcessorAffinityEx(PKAFFINITY_EX affinity, INT num);
 EXTERN_C VOID HalSendNMI(PKAFFINITY_EX affinity);
-
-PVOID p_stack_frames = NULL;
 
 /*
 Thread Information Block: (GS register)
@@ -43,49 +55,72 @@ Thread Information Block: (GS register)
 	...
 */
 
+NTSTATUS AnalyseStackWalk(_In_ int numCores)
+{	
+	for (int i = 0; i < numCores; i++)
+	{
+		NMI_CALLBACK_DATA thread_data;
+
+		RtlCopyMemory(
+			&thread_data,
+			(uintptr_t)thread_data_pool + i * sizeof(NMI_CALLBACK_DATA),
+			sizeof(NMI_CALLBACK_DATA)
+		);
+
+		DbgPrint("------------------------\n");
+		DbgPrint("kthread address: %llx", thread_data.kthread_address);
+		DbgPrint("Stack base: %llx\n", thread_data.stack_base);
+		DbgPrint("Stack limit: %llx\n", thread_data.stack_limit);
+		DbgPrint("start address: %llx\n", thread_data.start_address);
+		DbgPrint("cr3: %llx\n", thread_data.thread_cr3);
+		DbgPrint("stack frame pointer: %p\n", thread_data.stack_unwind_pool);
+		DbgPrint("num frames captured: %i\n", thread_data.num_frames_captured);
+
+		//do stuff
+
+		//free frame pool
+		ExFreePoolWithTag(thread_data.stack_unwind_pool, NMI_CB_POOL_TAG);
+	}
+}
+
 BOOLEAN NmiCallback(_In_ PVOID Context, _In_ BOOLEAN Handled)
 {
 	UNREFERENCED_PARAMETER(Context);
 	UNREFERENCED_PARAMETER(Handled);
 
-	DbgPrint("nmi callback called");
+	DbgPrint("nmi callback called\n");
 
-	/*
-	* GS register holds the address of the Thread Information Block
-	* We can use the __readgsqword function to read an address by offset
-	* from the TIB base
-	*/
-
-	PVOID TEB = (PVOID)__readgsqword(0x30);
-	DbgPrint("TEB address: %p", TEB);
-
-	PVOID current_thread = KeGetCurrentThread();
-	DbgPrint("Current thread: %p", current_thread);
-
-	//Start address can be spoofed but still good to check
-	UINT64 start_address = *((UINT64*)((uintptr_t)current_thread + 0x450));
+	//must free each pool after each stack frame has been analysed
+	PVOID stack_frames = ExAllocatePoolWithTag(NonPagedPool, 0x200, NMI_CB_POOL_TAG);	
 
 	int num_frames_captured = RtlCaptureStackBackTrace(
 		0,
-		0x400,
-		p_stack_frames,
+		0x200,
+		stack_frames,
 		NULL
 	);
 
-	DbgPrint("num frames captured: %i at address: %p", num_frames_captured, p_stack_frames);
+	PVOID current_thread = KeGetCurrentThread();
+	DbgPrint("Current thread: %p\n", current_thread);
 
-	for (size_t i = 0; i < num_frames_captured; i++)
-	{
-		DWORD64 temp_address = (DWORD64)((uintptr_t)p_stack_frames + i * 8);
+	NMI_CALLBACK_DATA thread_data = { 0 };
+	thread_data.kthread_address = (UINT64)current_thread;
+	thread_data.stack_base = *((UINT64*)((uintptr_t)current_thread + 0x030));
+	thread_data.stack_limit = *((UINT64*)((uintptr_t)current_thread + 0x038));
+	thread_data.start_address = *((UINT64*)((uintptr_t)current_thread + 0x450));		//can be spoofed but still a decent detection vector
+	thread_data.thread_cr3 = __readcr3();
+	thread_data.stack_unwind_pool = stack_frames;
+	thread_data.num_frames_captured = num_frames_captured;
 
-		//RtlCopyMemory(
-		//	&temp_address,
-		//	(uintptr_t)p_stack_frames + i * 8,
-		//	8
-		//);
+	DbgPrint("Current stack frame address: %p\n", stack_frames);
 
-		DbgPrint("stack: %llx\n", temp_address);
-	}
+	ULONG proc_num = KeGetCurrentProcessorNumber();
+
+	RtlCopyMemory(
+		((uintptr_t)thread_data_pool) + proc_num * sizeof(thread_data),
+		&thread_data,
+		sizeof(thread_data)
+	);
 
 	return TRUE;
 }
@@ -101,24 +136,20 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 	if (!ProcAffinityPool)
 		return STATUS_FAILED_DRIVER_ENTRY;
 
-	//Allocate pool for our stackwalk
-	p_stack_frames = ExAllocatePoolWithTag(NonPagedPool, 0x400, NMI_CB_POOL_TAG);
-
-	if (!p_stack_frames)
-		return STATUS_FAILED_DRIVER_ENTRY;
-
 	//Register our callback
 	PVOID NMICallbackHandle = KeRegisterNmiCallback(NmiCallback, 0);
 
 	if (!NMICallbackHandle)
 		return STATUS_FAILED_DRIVER_ENTRY;
 
+	//Count cores (NMI's are sent per core)
+	ULONG num_cores = KeQueryActiveProcessorCountEx(0);
+
+	thread_data_pool = ExAllocatePoolWithTag(NonPagedPool, num_cores * sizeof(NMI_CALLBACK_DATA), NMI_CB_POOL_TAG);
+
 	//Calculate our delay (200ms currently)
 	LARGE_INTEGER delay = { 0 };
 	delay.QuadPart -= 200 * 10000;
-
-	//Count cores (NMI's are sent per core)
-	ULONG num_cores = KeQueryActiveProcessorCountEx(0);
 
 	//Iterate over each logical processor to fire NMI
 	for (ULONG core = 0; core < num_cores; core++)
@@ -127,7 +158,7 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 		KeInitializeAffinityEx(ProcAffinityPool);
 		KeAddProcessorAffinityEx(ProcAffinityPool, core);
 
-		DbgPrint("Sending NMI");
+		DbgPrint("Sending NMI\n");
 
 		//Fire our NMI
 		HalSendNMI(ProcAffinityPool);
@@ -137,9 +168,12 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 		KeDelayExecutionThread(KernelMode, FALSE, &delay);
 	}
 
+	AnalyseStackWalk(num_cores);
+
 	//Unregister our callback + free allocated pool
 	KeDeregisterNmiCallback(NMICallbackHandle);
 	ExFreePoolWithTag(ProcAffinityPool, NMI_CB_POOL_TAG);
+	ExFreePoolWithTag(thread_data_pool, NMI_CB_POOL_TAG);
 
 	return STATUS_SUCCESS;
 }
